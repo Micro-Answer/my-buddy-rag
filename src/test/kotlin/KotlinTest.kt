6e9182a17a548c1653a83340662c7949ca9ed642
glass <silver20190204@gmail.com>
2025-01-10 11:31:34 +0900

    Let's go

---
+++ src/test/kotlin/KotlinTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class KotlinTest {
+
+    @Test
+    fun testAddition() {
+        val result = 2 + 3
+        assertEquals(5, result, "2 + 3 should equal 5")
+    }
+}
