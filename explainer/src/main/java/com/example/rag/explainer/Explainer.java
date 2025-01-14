ba91179ceafbc4d0126952946f2be5f361e8fceb
glass <silver20190204@gmail.com>
2025-01-14 12:29:45 +0900

    Add explainer subsystem

---
+++ explainer/src/main/java/com/example/rag/explainer/Explainer.java
@@ -0,0 +1,12 @@
+package com.example.rag.explainer;
+
+import core.explanation.ExplainerSystem;
+
+class Explainer implements ExplainerSystem {
+    @Override
+    public String explain(String contents, String personalData) {
+        String response = contents + "를 '" + personalData + "' 바탕으로 친절한 설명해주세요";
+        System.out.println(response);
+        return response;
+    }
+}
