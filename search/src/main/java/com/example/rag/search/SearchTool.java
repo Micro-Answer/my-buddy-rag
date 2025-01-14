200eabd48b5a5b2fef6dacee2d589db22e01964b
glass <silver20190204@gmail.com>
2025-01-14 12:26:42 +0900

    Add search subsystem

---
+++ search/src/main/java/com/example/rag/search/SearchTool.java
@@ -0,0 +1,13 @@
+package com.example.rag.search;
+
+import core.search.SearchSystem;
+
+class SearchTool implements SearchSystem {
+
+    @Override
+    public String search(String contents) {
+        String response = contents + "에 대해 검색했습니다";
+        System.out.println(response);
+        return response;
+    }
+}
