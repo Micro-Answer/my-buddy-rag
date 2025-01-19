2320117a3ae85d0e633bc8395e02cab1f0a5d875
glass <silver20190204@gmail.com>
2025-01-19 23:23:48 +0900

    Move files

--- qna/src/main/java/com/example/rag/qna/domain/Qna.java
+++ qna/src/main/java/com/example/rag/qna/domain/Qna.java
@@ -1,6 +1,5 @@
-package com.example.rag.qna;
+package com.example.rag.qna.domain;
 
-import core.qna.CacheSystem;
 import core.qna.QnaSystem;
 import core.rag.OpinionDTO;
 import core.rag.QuestionDTO;
@@ -8,8 +7,8 @@ import core.rag.QuestionTitleDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
 @Component
+@AllArgsConstructor
 class Qna implements QnaSystem {
 //    private final CacheSystem cache;
 
@@ -17,7 +16,7 @@ class Qna implements QnaSystem {
     public String enrollQuestion(String userId, String title, String category, String contents) {
         String response;
         synchronized (this) {
-            response = "질문을 등록했습니다 " + String.format("%s %s %s %s", userId, title, category, contents);
+            response = String.format("질문을 등록했습니다 %s %s %s %s", userId, title, category, contents);
 //            cache.putRecentQuestion("questionId", new Question(userId, "questionId", title, category, contents, "createdDate"));
         }
         System.out.println(response);
@@ -26,21 +25,21 @@ class Qna implements QnaSystem {
 
     @Override
     public String updateQuestion(String userId, String questionId, String category, String contents) {
-        String response = "질문을 수정했습니다 " + String.format("%s %s %s %s", userId, questionId, category, contents);
+        String response = String.format("질문을 수정했습니다 %s %s %s %s", userId, questionId, category, contents);
         System.out.println(response);
         return response;
     }
 
     @Override
     public String deleteQuestion(String userId, String questionId) {
-        String response = "질문을 삭제했습니다 " + String.format("%s %s", userId, questionId);
+        String response = String.format("질문을 삭제했습니다 %s %s", userId, questionId);
         System.out.println(response);
         return response;
     }
 
     @Override
     public QuestionDTO readQuestion(String questionId) {
-        String response = "질문을 조회했습니다 " + String.format("%s", questionId);
+        String response = String.format("질문을 조회했습니다 %s", questionId);
         System.out.println(response);
         return new Question("userId", "questionId", "title", "category", "contents", "createdDate");
 
@@ -48,7 +47,7 @@ class Qna implements QnaSystem {
 
     @Override
     public QuestionTitleDTO[] readQuestionTitles(String category, int startNum, int endNum) {
-        String response = "질문 목록을 조회했습니다 " + String.format("%s %d %d", category, startNum, endNum);
+        String response = String.format("질문 목록을 조회했습니다 %s %d %d", category, startNum, endNum);
         System.out.println(response);
         return new QuestionTitleDTO[]{
                 new QuestionTitle("questionId1", "title", "userId", "createdDate"),
@@ -59,34 +58,33 @@ class Qna implements QnaSystem {
 
     @Override
     public String enrollOpinion(String userId, String questionId, String title, String contents) {
-        String response = "의견을 등록했습니다 " + String.format("%s %s %s %s", userId, questionId, title, contents);
+        String response = String.format("의견을 등록했습니다 %s %s %s %s", userId, questionId, title, contents);
         System.out.println(response);
         return response;
     }
 
     @Override
     public String updateOpinion(String userId, String opinionId, String title, String contents) {
-        String response = "의견을 수정했습니다 " + String.format("%s %s %s %s", userId, opinionId, title, contents);
+        String response = String.format("의견을 수정했습니다 %s %s %s %s", userId, opinionId, title, contents);
         System.out.println(response);
         return response;
     }
 
     @Override
     public String deleteOpinion(String userId, String opinionId) {
-        String response = "의견을 삭제했습니다 " + String.format("%s %s", userId, opinionId);
+        String response = String.format("의견을 삭제했습니다 %s %s", userId, opinionId);
         System.out.println(response);
         return response;
     }
 
     @Override
     public OpinionDTO[] readOpinions(String questionId, int startNum, int endNum) {
-        String response = "의견들을 조회했습니다 " + String.format("%s %d %d", questionId, startNum, endNum);
+        String response = String.format("의견들을 조회했습니다 %s %d %d", questionId, startNum, endNum);
         System.out.println(response);
         return new OpinionDTO[] {
                 new Opinion("userId", "questionId", "opinionId1", "title", "contents", "createdDate"),
                 new Opinion("userId", "questionId", "opinionId2", "title", "contents", "createdDate"),
                 new Opinion("userId", "questionId", "opinionId3", "title", "contents", "createdDate")
-
         };
     }
 }
