c04d9d8b32a99a1efbf212b2444deaf28c601b87
glass <silver20190204@gmail.com>
2025-01-20 01:39:10 +0900

    Refactor SearchSystem

--- core/src/main/java/core/search/SearchSystem.java
+++ core/src/main/java/core/search/SearchSystem.java
@@ -1,5 +1,8 @@
 package core.search;
 
 public interface SearchSystem {
-    String search(String contents);
+    String search(String title, String category, String contents);
+    String enrollQuestion(String questionId, String title, String category, String contents);
+    String updateQuestion(String questionId, String title, String category, String contents);
+    String deleteQuestion(String questionId);
 }
