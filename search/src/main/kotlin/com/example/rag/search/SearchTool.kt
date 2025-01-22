09790c83ae128464bfff1b7e789ee27f4f90a655
glass <silver20190204@gmail.com>
2025-01-22 23:00:43 +0900

    Refactor with Kotlin

---
+++ search/src/main/kotlin/com/example/rag/search/SearchTool.kt
@@ -0,0 +1,27 @@
+package com.example.rag.search
+
+import core.search.SearchSystem
+import org.springframework.stereotype.Component
+
+@Component
+class SearchTool: SearchSystem {
+
+    override fun search(query: String): String
+        = query.also {
+            println("$it 검색했습니다")
+        }
+
+    override fun search(query: String, category: String): String
+        = query.also {
+            println("검색 문장 : $it, 카테고리 : $category . 검색했습니다")
+        }
+
+    override fun enrollQuestion(questionId: String, title: String, category: String, content: String): String
+        = "enrollQuestion"
+
+    override fun updateQuestion(questionId: String, title: String, category: String, content: String): String
+        = "updateQuestion"
+
+    override fun deleteQuestion(questionId: String): String
+        = "deleteQuestion"
+}
