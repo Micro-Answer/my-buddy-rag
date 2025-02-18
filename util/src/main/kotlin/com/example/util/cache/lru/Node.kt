8e4d8570c3d5a564ff97e6004389d4929d174e82
glass <silver20190204@gmail.com>
2025-02-18 18:40:41 +0900

    feat: 타임아웃 추가

--- util/src/main/kotlin/com/example/util/cache/lru/Node.kt
+++ util/src/main/kotlin/com/example/util/cache/lru/Node.kt
@@ -4,5 +4,9 @@ class Node<K, V>(
     val key: K,
     var value: V,
     var next: Node<K, V>? = null,
-    var prev: Node<K, V>? = null
-)
+    var prev: Node<K, V>? = null,
+    private val timeout: Timeout = Timeout()
+) {
+    fun isExpired(): Boolean =
+        timeout.isExpired()
+}
